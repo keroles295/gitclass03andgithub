3afe2503ec2adf3914218e78ab04a808981ba4ec
keroles295 <kerolesbotros295@gmail.com>
2022-06-01 09:56:56 -0400

    2st commit

--- src/newGitHubNew/GitandHub.java
+++ src/newGitHubNew/GitandHub.java
@@ -4,6 +4,9 @@ public class GitandHub {
 
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
+		System.out.println("First git hub");
+		System.out.println("Today is a nery good day");
+		
 
 	}
 
